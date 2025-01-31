bb658ea902132176d06c3fc226ec9fddd27509c1
Ilya <iovodovi@yandex.ru>
2025-01-31 09:47:26 +0300

    Добавлен роутинг запросов

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import express from 'express'
+import express from 'express';
+import { usersRouter } from './users/users.js';
 
 const port = '8000';
 const app = express();
 
 app.get('/hello', (req, res) => {
-	res.send('Привет, мир!');
+	res.send('Привет, мир');
 });
+
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
 	console.log(`Сервер запущен на http://localhost:${port}`)
