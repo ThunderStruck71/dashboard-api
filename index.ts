c96132df41f6c94cf3394ec7ad4d4150062c79c3
Ilya <iovodovi@yandex.ru>
2025-02-02 00:40:44 +0300

    Переход на TypeScript

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { usersRouter } from './users/users.js';
 
 const port = '8000';
@@ -9,6 +9,11 @@ app.get('/hello', (req, res) => {
 });
 
 app.use('/users', usersRouter);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	console.log(err.message);
+	res.status(401).send(err.message);
+})
 
 app.listen(port, () => {
 	console.log(`Сервер запущен на http://localhost:${port}`)
