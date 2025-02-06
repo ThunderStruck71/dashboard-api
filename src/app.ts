ed54c5ae1b4f2dbba29168e8dde750fe7a7d128b
Ilya <iovodovi@yandex.ru>
2025-02-06 10:41:25 +0300

    Добавил логгер

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, {Express} from 'express';
 import { usersRouter } from './users/users.js';
 import { Server } from 'http';
+import { LoggerService } from './logger/logger.service.js';
 
 export class App {
 	app: Express;
 	server: Server;
 	port: number;
+	logger: LoggerService;
 
-	constructor() {
+	constructor(logger: LoggerService) {
 		this.app = express();
 		this.port = 8080;
+		this.logger = logger;
 	}
 
 	private useRoutes() {
@@ -20,6 +23,6 @@ export class App {
 		this.useRoutes();
 
 		this.server = this.app.listen(this.port);
-		console.log(`Сервер запущен на http://localhost:${this.port}`);
+		this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
 	}
 }
---
+++ src/logger/logger.service.ts
@@ -0,0 +1,23 @@
+import { Logger, ILogObj, ISettingsParam } from 'tslog';
+
+export class LoggerService {
+	private logger: Logger<ILogObj>;
+
+	constructor() {
+		this.logger = new Logger<ILogObj>({
+			hideLogPositionForProduction: true
+		});
+	}
+
+	public error(...args: unknown[]) {
+		this.logger.error(...args);
+	}
+
+	public log(...args: unknown[]) {
+		this.logger.info(...args);
+	}
+
+	public warn(...args: unknown[]) {
+		this.logger.warn(...args);
+	}
+}
