9fd0e2b1c1a5bca849bb66b99895b0345a0209fd
Ilya <iovodovi@yandex.ru>
2025-02-06 12:48:40 +0300

    Добавил фильтр исключений

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, {Express} from 'express';
 import { Server } from 'http';
 import { LoggerService } from './logger/logger.service.js';
 import { UsersController } from './users/users.controller.js';
+import { ExceptionFilter } from './errors/exception.filter.js';
 
 export class App {
 	app: Express;
@@ -9,20 +10,27 @@ export class App {
 	port: number;
 	logger: LoggerService;
 	usersController: UsersController;
+	exceptionFilter: ExceptionFilter;
 
-	constructor(logger: LoggerService, usersController: UsersController) {
+	constructor(logger: LoggerService, usersController: UsersController, exceptionFilter: ExceptionFilter) {
 		this.app = express();
 		this.port = 8000;
 		this.logger = logger;
 		this.usersController = usersController;
+		this.exceptionFilter = exceptionFilter;
 	}
 
 	private useRoutes() {
 		this.app.use('/users', this.usersController.router);
 	}
 
+	private useExceptionFilters() {
+		this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+	}
+
 	public async init() {
 		this.useRoutes();
+		this.useExceptionFilters();
 
 		this.server = this.app.listen(this.port);
 		this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { App } from "./app.js";
+import { ExceptionFilter } from "./errors/exception.filter.js";
 import { LoggerService } from "./logger/logger.service.js";
 import { UsersController } from "./users/users.controller.js";
 
 async function bootstrap() {
 	const logger = new LoggerService();
-	const app = new App(logger, new UsersController(logger));
+	const app = new App(
+		logger, 
+		new UsersController(logger), 
+		new ExceptionFilter(logger)
+	);
 	await app.init();
 }
 
