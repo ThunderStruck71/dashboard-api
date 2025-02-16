0e6a5d23ad9219d618e74bbd918f3a4e118f6481
Ilya <iovodovi@yandex.ru>
2025-02-17 00:52:33 +0300

    Установил Logger как singleton

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ export interface IBootstrapReturn {
 }
 
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
-	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<ILogger>(TYPES.ILogger).to(LoggerService).inSingletonScope();
 	bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
 	bind<IUsersController>(TYPES.UsersController).to(UsersController);
 	bind<IUsersService>(TYPES.UsersService).to(UsersService);
